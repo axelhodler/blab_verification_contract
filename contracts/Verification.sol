e8b19b0ca8185ba36705727ab30cdfaf643609ae
Axel Hodler <derxor@gmail.com>
2016-08-21 14:16:00 +0200

    initial verification contract

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -1,3 +1,21 @@
 contract Verification {
+  mapping (string => Report) reports;
 
+  struct Report {
+    address[] validators;
+    bool isValid;
+  }
+
+  function verify(string reportId) {
+    if(!reports[reportId].isValid) {
+      reports[reportId].validators.push(msg.sender);
+    }
+    if(reports[reportId].validators.length == 3) {
+      reports[reportId].isValid = true;
+    }
+  }
+
+  function verifiersFor(string reportId) constant returns(address[] addresses) {
+    return reports[reportId].validators;
+  }
 }
--- test/metacoin.js
+++ test/metacoin.js
@@ -1,5 +1,11 @@
 contract('Verification', function(accounts) {
-  it('initializes', function() {
+  it('', function() {
     var meta = Verification.deployed();
+
+    return meta.verify('foo', {from: accounts[0]}).then(function() {
+      return meta.verifiersFor.call('foo').then(function(verifiers) {
+        assert.sameDeepMembers(verifiers, [accounts[0]]);
+      });
+    })
   });
 });
