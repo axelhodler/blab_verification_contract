d21317a3533ee62581e3189b50f68bbe63dfa318
Axel Hodler <derxor@gmail.com>
2016-09-17 20:58:43 +0200

    display infos about the contract

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,1 +1,17 @@
+window.onload = function() {
+  web3.eth.getAccounts(function(err, accs) {
+    if (err != null) {
+      alert("There was an error fetching your accounts.");
+      return;
+    }
 
+    if (accs.length == 0) {
+      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+      return;
+    }
+
+    document.getElementById('accounts').innerHTML = accs;
+    document.getElementById('contract-address').innerHTML = MetaCoin.deployed().address;
+    document.getElementById('contract-abi').innerHTML = JSON.stringify(MetaCoin.deployed().abi);
+  });
+};
