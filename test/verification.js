fbcff9b92f5d3a16f24989439b2b4408f46697c2
Axel Hodler <derxor@gmail.com>
2017-01-15 20:43:55 +0100

    use arrow functions

--- test/verification.js
+++ test/verification.js
@@ -1,39 +1,39 @@
-contract('Verification', function(accounts) {
+contract('Verification', (accounts) => {
   var contract;
   var alice = accounts[0];
   var bob = accounts[1];
   var carol = accounts[2];
 
-  function verify(reportId, account) {
+  var verify = (reportId, account) => {
     return contract.verify(reportId, {from: account});
-  }
+  };
 
-  function isValid(reportId) {
+  var isValid = (reportId) => {
     return contract.isValid.call(reportId);
-  }
+  };
 
-  function verifiersFor(reportId) {
+  var verifiersFor = (reportId) => {
     return contract.verifiersFor.call(reportId);
-  }
+  };
 
-  beforeEach(function() {
+  beforeEach(() => {
     contract = Verification.deployed();
   });
 
-  it('allows users to verify documents', function() {
-    return verify('DOCUMENT_HASH', alice).then(function() {
-      return verifiersFor('DOCUMENT_HASH').then(function(verifiers) {
+  it('allows users to verify documents', () => {
+    return verify('DOCUMENT_HASH', alice).then(() => {
+      return verifiersFor('DOCUMENT_HASH').then((verifiers) => {
         assert.equal(verifiers.length, 1);
         assert.sameDeepMembers(verifiers, [alice]);
       });
     })
   });
 
-  it('lists a document as valid after three verifications', function() {
-    return verify('2ND_DOCUMENT_HASH', alice).then(function() {
-      return verify('2ND_DOCUMENT_HASH', bob).then(function() {
-        return verify('2ND_DOCUMENT_HASH', carol).then(function() {
-          return isValid('2ND_DOCUMENT_HASH').then(function(contractIsValid) {
+  it('lists a document as valid after three verifications', () => {
+    return verify('2ND_DOCUMENT_HASH', alice).then(() => {
+      return verify('2ND_DOCUMENT_HASH', bob).then(() => {
+        return verify('2ND_DOCUMENT_HASH', carol).then(() => {
+          return isValid('2ND_DOCUMENT_HASH').then((contractIsValid) => {
             assert.isTrue(contractIsValid);
           })
         });
@@ -41,21 +41,21 @@ contract('Verification', function(accounts) {
     });
   });
 
-  it('cannot be verified by the same person twice', function() {
-    return verify('3ND_DOCUMENT_HASH', alice).then(function() {
-      return verify('3ND_DOCUMENT_HASH', alice).then(function() {
-        return verifiersFor('3ND_DOCUMENT_HASH').then(function(verifiers) {
+  it('cannot be verified by the same person twice', () => {
+    return verify('3ND_DOCUMENT_HASH', alice).then(() => {
+      return verify('3ND_DOCUMENT_HASH', alice).then(() => {
+        return verifiersFor('3ND_DOCUMENT_HASH').then((verifiers) => {
           assert.equal(verifiers.length, 1);
         });
       });
     });
   });
 
-  it('is not valid if the same person verifies it three times', function() {
-    return verify('4ND_DOCUMENT_HASH', alice).then(function() {
-      return verify('4ND_DOCUMENT_HASH', alice).then(function() {
-        return verify('4ND_DOCUMENT_HASH', alice).then(function() {
-          return isValid('4ND_DOCUMENT_HASH').then(function(contractIsValid) {
+  it('is not valid if the same person verifies it three times', () => {
+    return verify('4ND_DOCUMENT_HASH', alice).then(() => {
+      return verify('4ND_DOCUMENT_HASH', alice).then(() => {
+        return verify('4ND_DOCUMENT_HASH', alice).then(() => {
+          return isValid('4ND_DOCUMENT_HASH').then((contractIsValid) => {
             assert.isNotTrue(contractIsValid);
           })
         });
