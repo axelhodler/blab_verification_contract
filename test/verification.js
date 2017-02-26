7406f7108c0a91e73769c83ccc50cb3f6e20ca1f
Axel Hodler <derxor@gmail.com>
2017-02-26 09:15:11 +0100

    improve structure

--- test/verification.js
+++ test/verification.js
@@ -1,54 +1,54 @@
 var Verification = artifacts.require("./Verification.sol")
 
-contract('Verification', (accounts) => {
-  let contract;
-  let alice = accounts[0];
-  let bob = accounts[1];
-  let carol = accounts[2];
-  let NERO_THE_NONMEMBER = accounts[3];
+contract('Verification', accounts => {
+  let contract
+  let alice = accounts[0]
+  let bob = accounts[1]
+  let carol = accounts[2]
+  let NERO_THE_NONMEMBER = accounts[3]
 
   let verify = (reportId, account) => {
-    return contract.verify(reportId, {from: account});
-  };
+    return contract.verify(reportId, {from: account})
+  }
 
   let isValid = (reportId) => {
-    return contract.isValid.call(reportId);
-  };
+    return contract.isValid.call(reportId)
+  }
 
   let verifiersFor = (reportId) => {
-    return contract.verifiersFor.call(reportId);
-  };
+    return contract.verifiersFor.call(reportId)
+  }
 
   let submit = (reportId, account) => {
     return contract.submit(reportId, {from: account})
-  };
+  }
 
   beforeEach(() => {
     return Verification.deployed().then(instance => {
-      contract = instance;
-    });
-  });
+      contract = instance
+    })
+  })
 
   describe('submitting a report', () => {
-    const report = 'DOCUMENT_HASH_0';
+    const report = 'DOCUMENT_HASH_0'
 
     it('sets the submitter in the report', () => {
       return submit(report, alice).then(() => {
         return contract.submitterFor.call(report).then((submitter) => {
-          assert.equal(submitter, alice);
+          assert.equal(submitter, alice)
         })
       })
-    });
+    })
 
     it('is not allowed to submit the same report twice', done => {
       submit(report, alice).catch(() => {
-        assert.ok(true);
-        done();
+        assert.ok(true)
+        done()
       }).then(() => {
-        assert.fail();
-        done();
-      });
-    });
+        assert.fail()
+        done()
+      })
+    })
 
     it('is not allowed for nonmembers', done => {
       submit('NONMEMBER_REPORT', NERO_THE_NONMEMBER).catch(() => {
@@ -61,47 +61,49 @@ contract('Verification', (accounts) => {
     })
   })
 
-  it('allows users to verify documents', () => {
-    return submit('DOCUMENT_HASH', alice).then(() => {
-      return verifiersFor('DOCUMENT_HASH').then((verifiers) => {
-        assert.equal(verifiers.length, 0);
-        assert.sameDeepMembers(verifiers, []);
-      });
+  describe('verifying documents', () => {
+    it('allows users to verify documents', () => {
+      return submit('DOCUMENT_HASH', alice).then(() => {
+        return verifiersFor('DOCUMENT_HASH').then((verifiers) => {
+          assert.equal(verifiers.length, 0)
+          assert.sameDeepMembers(verifiers, [])
+        })
+      })
     })
-  });
 
-  it('lists a document as valid after two verifications', () => {
-    return submit('2ND_DOCUMENT_HASH', alice).then(() => {
-      return verify('2ND_DOCUMENT_HASH', bob).then(() => {
-        return verify('2ND_DOCUMENT_HASH', carol).then(() => {
-          return isValid('2ND_DOCUMENT_HASH').then((contractIsValid) => {
-            assert.isTrue(contractIsValid);
+    it('lists a document as valid after two verifications', () => {
+      return submit('2ND_DOCUMENT_HASH', alice).then(() => {
+        return verify('2ND_DOCUMENT_HASH', bob).then(() => {
+          return verify('2ND_DOCUMENT_HASH', carol).then(() => {
+            return isValid('2ND_DOCUMENT_HASH').then((contractIsValid) => {
+              assert.isTrue(contractIsValid)
+            })
           })
-        });
-      });
-    });
-  });
+        })
+      })
+    })
 
-  it('cannot be verified by the same person twice', () => {
-    return submit('3ND_DOCUMENT_HASH', alice).then(() => {
-      return verify('3ND_DOCUMENT_HASH', bob).then(() => {
+    it('cannot be verified by the same person twice', () => {
+      return submit('3ND_DOCUMENT_HASH', alice).then(() => {
         return verify('3ND_DOCUMENT_HASH', bob).then(() => {
-          return verifiersFor('3ND_DOCUMENT_HASH').then((verifiers) => {
-            assert.equal(verifiers.length, 1);
-          });
-        });
-      });
-    });
-  });
+          return verify('3ND_DOCUMENT_HASH', bob).then(() => {
+            return verifiersFor('3ND_DOCUMENT_HASH').then((verifiers) => {
+              assert.equal(verifiers.length, 1)
+            })
+          })
+        })
+      })
+    })
 
-  it('is not possible to verify an unsubmitted report', (done) => {
-    verify('5ND_DOCUMENT_HASH', alice)
-      .catch(() => {
-        assert.ok(true);
-        done();
-      }).then(() => {
-        assert.fail();
-        done();
-      });
-  });
-});
+    it('is not possible to verify an unsubmitted report', done => {
+      verify('5ND_DOCUMENT_HASH', alice)
+        .catch(() => {
+          assert.ok(true)
+          done()
+        }).then(() => {
+        assert.fail()
+        done()
+      })
+    })
+  })
+})
