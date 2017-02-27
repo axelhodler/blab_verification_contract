bd9bd091e817ccd04e80c2b10937d57eed118405
Axel Hodler <derxor@gmail.com>
2017-02-27 07:59:03 +0100

    since were dealing with two contracts now

--- bin/getabi.js
+++ bin/getabi.js
@@ -1,10 +1,18 @@
 var solc = require('solc')
 var fs = require('fs')
 
-var contract = fs.readFileSync('contracts/Verification.sol').toString()
-var compiled = solc.compile(contract, 1);
+var membershipContract = fs.readFileSync('contracts/Membership.sol').toString()
+var verificationContract = fs.readFileSync('contracts/Verification.sol').toString()
+
+var input = {
+  'Membership.sol': membershipContract,
+  'Verification.sol': verificationContract
+}
+
+var compiled = solc.compile({sources: input}, 1);
 
 for (var contractName in compiled.contracts) {
-  console.log(compiled.contracts[contractName].interface);
+  if (contractName === 'Verification')
+    console.log(compiled.contracts[contractName].interface);
 }
 
