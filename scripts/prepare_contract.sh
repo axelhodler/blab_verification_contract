31b9b3c70f494ea2d77ffa3fc5bb40f995779c9e
Axel Hodler <derxor@gmail.com>
2017-03-06 21:23:40 +0100

    getabi.js location has changed

--- scripts/prepare_contract.sh
+++ scripts/prepare_contract.sh
@@ -3,7 +3,7 @@
 docker run --rm -p 8545:8545 axelhodler/testrpc > testrpc.log 2>&1 &
 
 CONTRACT_ADDRESS=`./truffle.sh migrate | grep Verification: | awk '{print $2}'`
-CONTRACT_ABI=`node bin/getabi.js`
+CONTRACT_ABI=`node scripts/getabi.js`
 
 if [ ${#CONTRACT_ADDRESS} != 42 ]; then
   echo "Contract Address could not be resolved, exitting.."
