0d7b451fb9e1a9a968e9ae17de8a1ecfd5f0c451
Axel Hodler <derxor@gmail.com>
2017-03-11 10:57:49 +0100

    fix deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,11 @@ const Web3 = require('web3')
 const solc = require('solc')
 const fs = require('fs')
 
+const account = process.argv[2]
+const password = process.argv[3]
+
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+web3.personal.unlockAccount(account, password)
 
 var membershipContract = fs.readFileSync('contracts/Membership.sol').toString()
 var verificationContract = fs.readFileSync('contracts/Verification.sol').toString()
@@ -19,10 +23,11 @@ var compiled = solc.compile({sources: input}, 1);
 verificationContract = compiled.contracts.Verification
 
 const Verification = web3.eth.contract(JSON.parse(verificationContract.interface))
+
 Verification.new({
-  data: verificationContract.bytecode,
-  from: web3.eth.accounts[0],
-  gas: 1250000
+  data: '0x' + verificationContract.bytecode,
+  from: account,
+  gas: 2000000
 }, (err, contract) => {
   if (!err) {
     if (!contract.address) {
@@ -30,6 +35,8 @@ Verification.new({
     } else { // second callback
       console.log('contract address:\t' + contract.address)
     }
+  } else {
+    console.log(err)
   }
 })
 
