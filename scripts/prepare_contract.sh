b890eaffc775c9d7691e4050790122d5ef0fc828
Axel Hodler <derxor@gmail.com>
2017-05-14 20:59:09 +0200

    truffle output would mess up parsing

--- scripts/prepare_contract.sh
+++ scripts/prepare_contract.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 docker run --rm -p 8545:8545 axelhodler/testrpc > testrpc.log 2>&1 &
 
-./truffle.sh migrate
+./truffle.sh migrate &>/dev/null
 
 node scripts/addresses_and_abi.js
